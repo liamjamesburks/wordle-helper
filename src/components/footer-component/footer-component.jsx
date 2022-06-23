9c465a6ff62fc2a337b49e9575c56aa5d9877ad4
liamjamesburks <liamjamesburks@gmail.com>
2022-06-23 09:45:23 +0200

    Fixed links issue by replacing target with currentTarget.

--- src/components/footer-component/footer-component.jsx
+++ src/components/footer-component/footer-component.jsx
@@ -10,55 +10,70 @@ import 'react-toastify/dist/ReactToastify.css';
 const Footer = () => {
 
     const copyToClipboard = (event) => {
-        const id = event.target.id;
+        const id = event.currentTarget.id;
 
         let url;
         if (id.indexOf('#') >= 0) {
-            url = event.target.parentElement.id;
-            console.log(event.target.parentElement.id);
+            url = event.currentTarget.parentElement.id;
         } else {
-            url = event.target.id;
+            url = event.currentTarget.id;
         }
-        
-        navigator.clipboard.writeText(url).then(()=>{
-            if ((url.indexOf("liamjames") >= 0) || url.indexOf("helper") >= 0) {
-                toast("Link Copied to Clipboard",
-                    {
-                        position: "bottom-right",
-                        autoClose: 2000,
-                        hideProgressBar: true,
-                        closeOnClick: true,
-                        pauseOnHover: false,
-                        draggable: false,
-                        progress: undefined
-                    });
-            } else {
-                window.open(url);
-            }
-        })
+
+        navigator.clipboard.writeText(url)
+            .then(
+                ()=>{
+                    console.log(url);
+
+                    if ((url.indexOf("liamjames") >= 0) || url.indexOf("helper") >= 0) {
+                        toast("Link Copied to Clipboard",
+                            {
+                                position: "bottom-right",
+                                autoClose: 2000,
+                                hideProgressBar: true,
+                                closeOnClick: true,
+                                pauseOnHover: false,
+                                draggable: false,
+                                progress: undefined
+                            });
+                    } else if (!url) {
+                        toast("Something Went Wrong, Try Again",
+                            {
+                                position: "bottom-right",
+                                autoClose: 2000,
+                                hideProgressBar: true,
+                                closeOnClick: true,
+                                pauseOnHover: false,
+                                draggable: false,
+                                progress: undefined
+                            });
+                    } else {
+                        window.open(url);
+                    }
+                }
+            )
     }
 
     return (
         <div className="footer-container">
             <div className="centred">
-                <h6 className="larger lighter"><strong>Copyright &copy;</strong> Liam Burks</h6>
+                <h6 className="larger lighter"><strong className="mr-2">&copy; Created By </strong> Liam Burks</h6>
             </div>
             <span className="line-bigger" />
 
             <div id="liamjamesburks@gmail.com" className="footer-item-container" onClick={copyToClipboard}>
-                <p id="p#1" className="lighter footer-item-detail link">My Email</p>
+                <div id="p#1" className="lighter footer-item-detail link">My Email</div>
                 <DuplicateIcon id="i#1" className="footer-icon"/>
             </div>
 
             <div id="https://www.nytimes.com/games/wordle/index.html" className="footer-item-container" onClick={copyToClipboard}>
-                <p id="p#3" className="lighter footer-item-detail link">Wordle</p>
-                <GlobeIcon id="i#3" name="https://www.nytimes.com/games/wordle/index.html" className="footer-icon"/>
+                <div id="p#3" className="lighter footer-item-detail link">Wordle</div>
+                <GlobeIcon id="i#3" className="footer-icon"/>
             </div>
 
             <span className="line" />
             <div id="https://wordle-helper-app.netlify.app/" className="footer-item-container" onClick={copyToClipboard}>
-                <p id="p#3" className="lighter footer-item-detail link">Share Us With Your Friends</p>
-                <LinkIcon id="i#3" name="https://wordle-helper-app.netlify.app/" className="footer-icon"/>
+                <div id="p#3" className="lighter footer-item-detail link">Share This With Your Friends</div>
+                <LinkIcon id="i#3" className="footer-icon"/>
             </div>
 
             <ToastContainer
